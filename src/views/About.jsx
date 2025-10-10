6ab014fc5f95bb9d5812be6ee68ef4f5547e0431
Whisperer <whisperer.meta@gmail.com>
2025-10-10 15:32:46 +0300

    Add About page

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,7 +1,62 @@
 import React from 'react'
+import useLocale from '../hooks/useLocale'
+import useIcons from '../hooks/useIcons';
 
 export default function About() {
+  const locale = useLocale();
+  const dateOfBirth = new Date(2003, 12, 16);
+  const today = new Date();
+  const age = today.getFullYear() - dateOfBirth.getFullYear();
+
+  const { phone, email } = useIcons();
+
   return (
-    <div>About</div>
-  )
+		<>
+			<h2>{locale.personalInformation}</h2>
+			<span className="tag">{locale.location}</span>
+			<span className="tag">
+				{age} {locale.age}
+			</span>
+
+			<div className="block">
+				<h3>Herkus Žilaitis</h3>
+
+				<div className="phoneNumber">
+					<span className="icon">{phone}</span>
+					<span>+37063285735</span>
+				</div>
+
+				<div className="email">
+					<span className="icon">{email}</span>
+					<span>whisperer.meta@gmail.com</span>
+				</div>
+			</div>
+
+			<span className="tag">{locale.bio}</span>
+			<p className='paragraph'>{locale.bioText}</p>
+
+			<h2>{locale.education}</h2>
+			<span className="tag">{locale.kaunasCollegeOfTechnology}</span>
+			<div className="block">
+				<h3>{locale.fieldOfStudy}</h3>
+				<span>{locale.bachelorDegree}</span>
+			</div>
+
+			<span className="tag">
+				{locale.kaunasSchoolOfInformationTechnologies}
+			</span>
+			<div className="block">
+				<h3>{locale.fieldOfStudyHighSchool}</h3>
+				<span>{locale.highSchoolDegree}</span>
+      </div>
+      
+      <h2>{locale.languages}</h2>
+      <ul className="languages">
+        <li><span className="tag">{locale.english}</span></li>
+        <li><span className="tag">{locale.lithuanian}</span></li>
+        <li><span className="tag">{locale.russian}</span></li>
+        <li><span className="tag">{locale.dutch}</span></li>
+      </ul>
+		</>
+	);
 }
